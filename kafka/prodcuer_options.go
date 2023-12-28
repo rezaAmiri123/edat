6b137cbb139e178084ef8a6cf4ff755648ee3df7
Reza Amiri <reza.amiri.ac@gmail.com>
2023-12-28 18:00:30 +0300

    add kafka

---
+++ kafka/prodcuer_options.go
@@ -0,0 +1,26 @@
+package edatkafka
+
+import (
+	"github.com/rezaAmiri123/edat/log"
+	"github.com/segmentio/kafka-go"
+)
+
+type ProducerOption func(producer *Producer)
+
+func WithProducerSerializer(serializer Serializer) ProducerOption {
+	return func(producer *Producer) {
+		producer.serializer = serializer
+	}
+}
+
+func WithProducerLogger(logger log.Logger) ProducerOption {
+	return func(producer *Producer) {
+		producer.logger = logger
+	}
+}
+
+func WithProducerTransport(transport *kafka.Transport) ProducerOption {
+	return func(producer *Producer) {
+		producer.writer.Transport = transport
+	}
+}
---
+++ pgx/client.go
@@ -0,0 +1,7 @@
+package edatpgx
+
+import (
+_	"github.com/jackc/pgconn"
+	_"github.com/jackc/pgx/v4"
+)
+// Client covers a subset of what both pgx.Conn or pgxpool.Pool provide
